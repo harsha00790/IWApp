3b62217e9535e3d95217c51c4456e2154999f4ac
harsha00790 <harsha00790@gmail.com>
2016-05-17 09:22:38 +0530

    Server outline -- 1

---
+++ server/js/logparams.js
@@ -0,0 +1,24 @@
+/**
+ * Created by rlakkoju on 5/17/2016.
+ */
+
+/*
+* The interface which gives the client the offset, size and type information given the Logname and the parameter string
+* The logs info is assumed to be stored in the MongoDB, considering we would be having a JSON --
+*/
+
+var IWApp = typeof(IWApp == "undefined")?({}):IWApp;
+
+IWApp.LogsInfo = {
+    GetDetails: function(logName, parameterName){
+
+        // -- Returns something like this --
+        return {
+            offset: 0,
+            length: 0,
+            type: "DOUBLE",
+            collectionOffset: 0,
+            collectionSize: 0
+        }
+    },
+};
